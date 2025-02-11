99657f94e7543082f278cc57c8c7c1dd57b3dd61
Arancreat <arancreat@gmail.com>
2025-02-11 19:36:31 +0900

    Добавьте файлы проекта.

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace NiichanWordCloudGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
